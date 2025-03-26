5f7548c051d4f2645bfdd86fd3f2d3b2b7d7d92b
arch3rPro <30855883+arch3rPro@users.noreply.github.com>
2025-03-26 13:23:03 +0800

    Create VPDoc.vue

---
+++ .vitepress/theme/components/VPDoc.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useData } from 'vitepress'
+import { ref, onMounted } from 'vue'
+import DefaultTheme from 'vitepress/theme'
+
+const { page } = useData()
+const VPDoc = DefaultTheme.Layout.VPDoc
+</script>
+
+<template>
+  <VPDoc>
+  </VPDoc>
+</template>
+
+<style scoped>
+:deep(.content-container),
+:deep(.content) {
+  max-width: 100% !important;
+}
+</style>

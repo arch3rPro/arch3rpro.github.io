2bc85b1a84496f0a6a7a27081b9a64db35b231f6
arch3rPro <30855883+arch3rPro@users.noreply.github.com>
2025-03-26 15:06:18 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import Components from 'unplugin-vue-components/vite'
 import { ArcoResolver } from 'unplugin-vue-components/resolvers'
-import path from 'path'
 
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            'vitepress/theme/components/VPDoc.vue': path.resolve(__dirname, '.vitepress/theme/components/VPDoc.vue')
-        }
-    },
     plugins: [
         Components({
             dirs: ['.vitepress/theme/components'],

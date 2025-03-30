5699ea74f1d1b96b71baa21f28d18043ec6dd21b
arch3rPro <30855883+arch3rPro@users.noreply.github.com>
2025-03-30 19:21:35 +0800

    Update data.ts

--- pages/nav/data.ts
+++ pages/nav/data.ts
@@ -2,108 +2,453 @@ import type { NavData } from '../../.vitepress/theme/types'
 
 export const NAV_DATA: NavData[] = [
   {
-    title: '常用工具',
+    title: '入门推荐',
     items: [
       {
-        icon: 'https://tinypng.com/images/apple-touch-icon.png',
-        title: 'TinyPNG',
-        desc: '在线图片压缩工具',
-        link: 'https://tinypng.com'
+        icon: '',
+        title: 'Github',
+        desc: 'Github开源内容丰富，学会资源查找，是成功踏入门槛的第一步',
+        link: 'https://github.com/',
+        badge: '强烈推荐'
       },
       {
-        icon: 'https://devtool.tech/logo.svg',
-        title: '开发者武器库',
-        desc: '开发者武器库，做开发者最专业最好用的专业工具箱',
-        link: 'https://devtool.tech'
+        icon: '',
+        title: 'HACKing信息流',
+        desc: '干净的信息流推送工具，偏向安全圈的点点滴滴，为安全研究人员每日发现优质内容。',
+        link: 'https://i.hacking8.com/'
       },
       {
-        icon: 'https://tool.lu/favicon.ico',
-        title: '在线工具',
-        desc: '开发人员的工具箱',
-        link: 'https://tool.lu'
+        icon: '',
+        title: 'ISC安全课',
+        desc: '网络安全和AI体系课程，博主出品，不一定是精品，嘿嘿',
+        link: 'https://study.360.net/',
+        badge: '推荐'
       },
       {
-        icon: 'https://www.sojson.com/sojson/favicon16.png',
-        title: 'SoJson在线解析',
-        desc: 'JSON在线解析及格式化验证',
-        link: 'https://www.sojson.com'
+        icon: '',
+        title: 'SecWiki安全技能树',
+        desc: '了解常见安全职位需要具备什么样的能力和技术',
+        link: 'https://www.sec-wiki.com/skill/index'
+      },
+      {
+        icon: '',
+        title: '安全技能树简版',
+        desc: '安全技能树简版',
+        link: 'http://evilcos.me/security_skill_tree_basic/index.html'
+      },
+      {
+        icon: '',
+        title: '技能时间轴',
+        desc: '技能时间轴',
+        link: 'http://www.anquanquan.info/sifangcai/tuijian/jinengzhou.pdf'
+      },
+      {
+        icon: '',
+        title: '漏洞银行技能树',
+        desc: '漏洞银行技能树',
+        link: 'https://skills.bugbank.cn/'
+      },
+      {
+        icon: '',
+        title: '习科技能表',
+        desc: '习科技能表',
+        link: 'http://www.shentoushi.top/Public/upload/images/2017-07-09/Sisso_skills_table.jpg'
+      },
+      {
+        icon: '',
+        title: '信息安全思维导图',
+        desc: '安全思维导图集合',
+        link: 'https://github.com/SecWiki/sec-chart'
+      },
+      {
+        icon: '',
+        title: '知道创宇研发技能表v3.1',
+        desc: '知道创宇研发技能表v3.1',
+        link: 'https://blog.knownsec.com/Knownsec_RD_Checklist/index.html'
       }
     ]
   },
   {
-    title: '开发参考',
+    title: '在线学习',
     items: [
       {
-        icon: {
-          svg: '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" height="1em" width="1em"><path d="m21.66 10.44-.98 4.18c-.84 3.61-2.5 5.07-5.62 4.77-.5-.04-1.04-.13-1.62-.27l-1.68-.4c-4.17-.99-5.46-3.05-4.48-7.23l.98-4.19c.2-.85.44-1.59.74-2.2 1.17-2.42 3.16-3.07 6.5-2.28l1.67.39c4.19.98 5.47 3.05 4.49 7.23Z" fill="#c9d1d9"></path><path d="M15.06 19.39c-.62.42-1.4.77-2.35 1.08l-1.58.52c-3.97 1.28-6.06.21-7.35-3.76L2.5 13.28c-1.28-3.97-.22-6.07 3.75-7.35l1.58-.52c.41-.13.8-.24 1.17-.31-.3.61-.54 1.35-.74 2.2l-.98 4.19c-.98 4.18.31 6.24 4.48 7.23l1.68.4c.58.14 1.12.23 1.62.27Zm2.43-8.88c-.06 0-.12-.01-.19-.02l-4.85-1.23a.75.75 0 0 1 .37-1.45l4.85 1.23a.748.748 0 0 1-.18 1.47Z" fill="#228e6c"></path><path d="M14.56 13.89c-.06 0-.12-.01-.19-.02l-2.91-.74a.75.75 0 0 1 .37-1.45l2.91.74c.4.1.64.51.54.91-.08.34-.38.56-.72.56Z" fill="#228e6c"></path></svg>'
-        },
-        title: 'Quick Reference',
-        desc: '为开发人员分享快速参考备忘清单【速查表】',
-        link: 'https://wangchujiang.com/reference',
-        badge: '开源'
+        icon: '',
+        title: '51CTO学院',
+        desc: 'IT在线教育,IT在线培训,IT培训,IT精品课,Python,大数据,区块链',
+        link: 'https://edu.51cto.com/'
       },
       {
-        icon: 'https://gorse.io/logo.png',
-        title: 'Gorse',
-        desc: '一个使用Go语言开发的开源推荐系统',
-        link: 'https://gorse.io/zh',
-        badge: '开源'
+        icon: '',
+        title: 'BiliBili',
+        desc: 'B站学习安全关注UP',
+        link: 'https://space.bilibili.com/37422870'
       },
       {
-        icon: './favicon.ico',
-        title: 'AST Explorer',
-        desc: '一个在线的AST（抽象语法树）查看器',
-        link: 'https://astexplorer.net',
-        badge: '开源'
+        icon: '',
+        title: 'Freebuf公开课',
+        desc: 'FreeBuf黑客与极客,国内关注度最高的全球互联网安全媒体平台,同时也是爱好者们交流与分享安全技术的最佳社区。',
+        link: 'https://live.freebuf.com/'
       },
-    ]
-  },
-  {
-    title: '推荐博客',
-    items: [
       {
-        icon: 'https://avatars.githubusercontent.com/u/9117028?v=4',
-        title: 'Kaito\'s Blog',
-        desc: '一位资深后端，涉及领域有 Redis、中间件、异地多活、K8s、云原生',
-        link: 'http://kaito-kidd.com',
-        badge: 'kaito'
+        icon: '',
+        title: 'Hack Inn-ppt分享',
+        desc: 'Hack Inn-PPT安全技术分享',
+        link: 'http://www.hackinn.com'
+      },
+      {
+        icon: '',
+        title: 'i春秋',
+        desc: '【i春秋】-专注网络安全_信息安全_白帽子的在线学习_教育_培训平台',
+        link: 'https://www.ichunqiu.com/'
+      },
+      {
+        icon: '',
+        title: '菜鸟教程',
+        desc: '菜鸟教程 - 学的不仅是技术，更是梦想',
+        link: 'http://www.runoob.com'
+      },
+      {
+        icon: '',
+        title: '工控安全资料共享库',
+        desc: '工控安全资料共享库',
+        link: 'https://github.com/Gh05ter/Bank'
+      },
+      {
+        icon: '',
+        title: '国外学习-blackmoreops',
+        desc: 'Linux，InfoSec，黑客-blackMORE Ops',
+        link: 'https://www.blackmoreops.com/'
+      },
+      {
+        icon: '',
+        title: '国外学习-securitytube',
+        desc: 'securitytube',
+        link: 'http://www.securitytube.net/'
+      },
+      {
+        icon: '',
+        title: '看雪KSSD',
+        desc: '看雪KSSD',
+        link: 'http://www.pediy.com/kssd/index.html'
+      },
+      {
+        icon: '',
+        title: '墨者学院',
+        desc: '专注于网络安全人才培养',
+        link: 'https://mozhe.cn'
+      },
+      {
+        icon: '',
+        title: '中国大学MOOC',
+        desc: '中国大学MOOC(慕课)_最好的在线课程学习平台',
+        link: 'http://www.icourse163.org'
       }
     ]
   },
   {
-    title: '资源收藏',
+    title: '安全博客',
     items: [
       {
-        icon: {
-          svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"></path></svg>'
-        },
-        title: 'Wechat Versions',
-        desc: 'Windows微信历史版本收集仓库',
-        link: 'https://github.com/tom-snow/wechat-windows-versions',
+        icon: '',
+        title: '360安全技术博客',
+        desc: '360核心安全技术博客',
+        link: 'http://blogs.360.cn'
+      },
+      {
+        icon: '',
+        title: '4ido10n',
+        desc: '4ido10n博客',
+        link: 'https://hackfun.org/'
+      },
+      {
+        icon: '',
+        title: 'ApiCoder Blog',
+        desc: 'next,python,渗透测试,web安全,Fi9Coder,信息安全,代码审计',
+        link: 'https://www.safeinfo.me/'
+      },
+      {
+        icon: '',
+        title: 'Black-Hole博客',
+        desc: 'Black-Hole | 一个脱离了高级趣味的恋爱狗',
+        link: 'http://www.bugs.cc'
       }
     ]
   },
   {
-    title: 'GG棒',
+    title: '信息收集',
     items: [
       {
-        title: 'UnitedShop',
-        desc: '可以看看老外在卖啥',
-        link: 'https://unitedshop.su/usercp/auth/login',
-        badge: 'Mark'
+        icon: '',
+        title: '云悉',
+        desc: '在线CMS识别',
+        link: 'http://www.yunsee.cn/'
       },
       {
-        title: 'Eneba',
-        desc: '免登录买大饼礼品卡',
-        link: 'https://www.eneba.com/vendor/azteco',
-        badge: 'Mark'
+        icon: '',
+        title: '云悉指纹平台',
+        desc: '云悉WEB资产梳理|在线CMS指纹识别平台',
+        link: 'http://www.yunsee.cn'
       },
       {
-        title: 'TGWiki',
-        desc: '有Tg使用的问题或许可以在这里找到方案',
-        link: 'https://tgnav.github.io/tgwiki',
-        badge: 'Mark'
+        icon: '',
+        title: '在线nmap',
+        desc: '在线nmap扫描端口',
+        link: 'http://nmap.online-domain-tools.com/'
       },
+      {
+        icon: '',
+        title: '在线漏洞扫描器',
+        desc: '在线漏洞扫描器|Online Vulnerability Scanners',
+        link: 'https://hackertarget.com'
+      },
+      {
+        icon: '',
+        title: '在线旁站查询',
+        desc: '在线旁站查询|C段查询|必应接口C段查询',
+        link: 'https://phpinfo.me/bing.php'
+      },
+      {
+        icon: '',
+        title: '在线域名Whois查询',
+        desc: '在线域名Whois查询',
+        link: 'http://whois.bugscaner.com'
+      },
+      {
+        icon: '',
+        title: '在线指纹识别',
+        desc: '在线cms指纹识别',
+        link: 'http://whatweb.bugscaner.com/look'
+      },
+      {
+        icon: '',
+        title: '重大网站在线监测',
+        desc: '重大网站在线监测',
+        link: 'http://0day.websaas.com.cn'
+      }
+    ]
+  },
+  {
+    title: '在线靶机',
+    items: [
+      {
+        icon: '',
+        title: 'Attack-Defense',
+        desc: 'Attack-Defense Online Lab',
+        link: 'https://attackdefense.com/'
+      },
+      {
+        icon: '',
+        title: 'AWVS测试网站',
+        desc: 'AWVS漏洞扫描测试网站',
+        link: 'http://vulnweb.com/'
+      },
+      {
+        icon: '',
+        title: 'bWAPP在线靶场',
+        desc: 'bWAPP在线靶场',
+        link: 'https://bwapp.hakhub.net/login.php'
+      },
+      {
+        icon: '',
+        title: 'DVWA',
+        desc: '在线DVWA靶机',
+        link: 'http://dvwa.bihuo.cn/login.php'
+      },
+      {
+        icon: '',
+        title: 'Exploit Exercises',
+        desc: '提供了各种虚拟机，文档和挑战',
+        link: 'https://exploit-exercises.com'
+      },
+      {
+        icon: '',
+        title: 'Vulapps靶机',
+        desc: '快速搭建各种漏洞环境(Various vulnerability environment)',
+        link: 'https://vulapps.evalbug.com/'
+      },
+      {
+        icon: '',
+        title: 'vulfocus',
+        desc: 'vulfocus-fofa漏洞复现',
+        link: 'http://vulfocus.fofa.so/'
+      },
+      {
+        icon: '',
+        title: 'Vulhub靶场',
+        desc: '使用Vulhub一键搭建漏洞测试靶场',
+        link: 'https://vulhub.org/'
+      },
+      {
+        icon: '',
+        title: 'Vulnhub靶场',
+        desc: 'Vulnhub靶场-虚拟靶机下载网站',
+        link: 'https://www.vulnhub.com/'
+      },
+      {
+        icon: '',
+        title: 'VULNSPY',
+        desc: 'Security\'s Demos Archive',
+        link: 'https://www.vulnspy.com/'
+      }
+    ]
+  },
+  {
+    title: 'APT攻防',
+    items: [
+      {
+        icon: '',
+        title: 'ATT＆CK中文矩阵',
+        desc: 'MITRE ATT＆CK-ATT＆CK中文矩阵列表',
+        link: 'https://www.shentoushi.top/att/'
+      },
+      {
+        icon: '',
+        title: 'C2 Matrix',
+        desc: 'The C2 Matrix',
+        link: 'https://howto.thec2matrix.com/'
+      },
+      {
+        icon: '',
+        title: 'CheatSheets',
+        desc: 'Introduction - CheatSheets',
+        link: 'https://cheats.philkeeble.com/'
+      },
+      {
+        icon: '',
+        title: 'Cobalt Strike',
+        desc: '基础部分,扩展进阶,原理部分',
+        link: 'https://wbglil.gitbook.io/cobalt-strike/'
+      },
+      {
+        icon: '',
+        title: 'GTFOBins',
+        desc: 'GTFOBins是一个精选的Unix二进制列表，攻击者可以利用它来绕过本地安全限制。',
+        link: 'https://gtfobins.github.io/'
+      },
+      {
+        icon: '',
+        title: 'LOLBAS',
+        desc: 'Living Off The Land Binaries and Scripts (and also Libraries)',
+        link: 'https://lolbas-project.github.io/'
+      },
+      {
+        icon: '',
+        title: 'MITRE ATT&AK&tm',
+        desc: 'MITRE ATT&AK&tm',
+        link: 'https://attack.mitre.org/'
+      }
+    ]
+  },
+  {
+    title: 'CTF竞赛',
+    items: [
+      {
+        icon: '',
+        title: 'Bugku CTF',
+        desc: 'Bugku CTF',
+        link: 'https://ctf.bugku.com/'
+      },
+      {
+        icon: '',
+        title: 'BUUCTF',
+        desc: 'BUUCTF，北京联合大学',
+        link: 'https://buuoj.cn/'
+      },
+      {
+        icon: '',
+        title: 'CTFHub',
+        desc: '开箱即用的CTF学习解决方案',
+        link: 'https://www.ctfhub.com'
+      },
+      {
+        icon: '',
+        title: 'CTFtime',
+        desc: 'There are a lot of Capture The Flag (CTF) competitions in our days, some of them have excelent tasks, but in most cases they\'re forgotten just after the CTF finished',
+        link: 'https://ctftime.org/'
+      },
+      {
+        icon: '',
+        title: '腾讯游戏安全',
+        desc: '腾讯游戏安全',
+        link: 'https://gamesafe.qq.com/index.shtml'
+      },
+      {
+        icon: '',
+        title: 'XCTF',
+        desc: 'XCTF联赛，网络安全竞赛，信息安全，CTF赛事交流，信息安全竞赛学习，白帽黑客论坛，网络安全人才聚集地，白帽黑客大本营',
+        link: 'https://adworld.xctf.org.cn/'
+      }
+    ]
+  },
+  {
+    title: '优质文章',
+    items: [
+      {
+        icon: '',
+        title: 'Tide安全团队',
+        desc: 'Tide安全团队-语雀技术文章',
+        link: 'https://www.yuque.com/tidesec/wiki'
+      },
+      {
+        icon: '',
+        title: '暗月知识中心',
+        desc: '暗月知识分享中心',
+        link: 'https://wiki.moonteams.com/'
+      },
+      {
+        icon: '',
+        title: '白阁文库',
+        desc: '白泽Sec团队维护的一个漏洞POC和EXP披露以及漏洞复现的开源项目',
+        link: 'https://wiki.bylibrary.cn/'
+      },
+      {
+        icon: '',
+        title: '狼组知识库',
+        desc: '狼组安全团队公开知识库-致力于打造信息安全乌托邦',
+        link: 'https://wiki.wgpsec.org/'
+      },
+      {
+        icon: '',
+        title: '收集质量安全文章',
+        desc: '优质安全文章收藏长期更新',
+        link: 'https://github.com/tom0li/collection-document'
+      },
+      {
+        icon: '',
+        title: '信安之路',
+        desc: '分享信息安全技术学习的平台',
+        link: 'https://mp.weixin.qq.com/s/Kp7X0Fac-wfWTqeXJG8Jxw'
+      }
+    ]
+  },
+
+  {
+    title: '知识分享',
+    items: [
+      {
+        icon: '',
+        title: 'Kail渗透艺术',
+        desc: 'Kail Linux渗透测试的艺术',
+        link: 'https://jobrest.gitbooks.io/kali-linux-cn/content'
+      },
+      {
+        icon: '',
+        title: 'Kali中文说明书',
+        desc: 'Kali Linux 工具使用中文说明书',
+        link: 'https://www.shentoushi.top/kali-tools/tools_zh.html'
+      },
+      {
+        icon: '',
+        title: 'Linux命令大全',
+        desc: 'Linux命令大全(手册)_Linux常用命令行实例详解_Linux命令学习手册',
+        link: 'http://man.linuxde.net'
+      },
+      {
+        icon: '',
+        title: 'Linux命令搜索引擎',
+        desc: 'Linux命令搜索引擎:最专业的Linux命令大全',
+        link: 'https://www.shentoushi.top/linux-command/'
+      }
     ]
   }
 ]
